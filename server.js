4ce9c2d4c3f6d0fb4d96ce7c30d97f970d09d9f2
Jay Williams <course@jaywilliams.me>
2017-11-08 20:07:11 +0000

    Whoop

--- server.js
+++ server.js
@@ -3,15 +3,28 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.use(express.static("assets"))
+
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/screen', function(req, res, next) {
     res.sendFile(__dirname + '/html/index.html');
 });
+app.get('/nessa', function(req, res, next) {
+    res.sendFile(__dirname + '/html/nessa.html');
+});
+app.get('/islam', function(req, res, next) {
+    res.sendFile(__dirname + '/html/islam.html');
+});
+var canPress = true;
 io.on('connection', function(socket) {
     console.log('Client connected...');
-    setTimeout(() => {
-        socket.emit("team", Math.random() > 0.5 ? "Nessa" : "Islam");
-    }, 2000)
+    socket.on("plsBuzz", function (team) {
+        if (canPress) {
+            canPress = !canPress;
+            setTimeout(function() { canPress = true; }, 2000);
+            io.emit("buzz", team);
+        } 
+    })
 
 })
 server.listen(4200);
